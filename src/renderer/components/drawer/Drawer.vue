05dd18fee905ecb172c928f30e84bfa1e342d180
Joanne Huang <prince811009@gmail.com>
2019-07-14 14:28:19 +0800

    modify src

--- src/renderer/components/drawer/Drawer.vue
+++ src/renderer/components/drawer/Drawer.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="Drawer">
+  <div class="timeMissionWrap">
+    <app-Timer-Mission />
+  </div>
     <transition
       name="fade"
       mode="out-in"
@@ -7,28 +10,39 @@
       <component :is="currentDrawer" />
     </transition>
     <app-drawer-menu />
+    <div class="titleBar"><app-titlebar /></div>
+    <div class="closeWin"
+    @click="toggleDrawer"
+    >
+      <img src="../../assets/close_white.png"
+      @click="toggleDrawer"
+      >
+    </div>
   </div>
 </template>
 
 <script>
-import appDrawerMenu from '@/components/drawer/Drawer-menu'
-// import appDrawerAbout from '@/components/drawer/Drawer-about'
-import appDrawerSettings from '@/components/drawer/Drawer-settings'
 import appDrawerTimer from '@/components/drawer/Drawer-timer'
+import appTimerMission from '@/components/timer/Timer-mission'
+import appTitlebar from '@/components/Titlebar'
 
 export default {
   name: 'Drawer',
 
   components: {
-    appDrawerMenu,
-    // appDrawerAbout,
-    appDrawerSettings,
-    appDrawerTimer
+    appDrawerTimer,
+    appTimerMission,
+    appTitlebar
   },
 
   computed: {
     currentDrawer() {
       return this.$store.getters.currentDrawer
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.$store.dispatch('toggleDrawer')
     }
   }
 }
@@ -36,11 +50,34 @@ export default {
 
 <style lang="scss" scoped>
 .Drawer {
-  background-color: $colorLightNavy;
+  background-color: #003164;
   position: relative;
   width: 100%;
-  height: calc(100% - 68px);
+  min-height: 100%;
+  margin-top: -68px;
+  padding: 45px 0px;
   z-index: 21;
   -webkit-app-region: no-drag;
+}
+
+.timeMissionWrap {
+  width: 445px;
+  position: absolute;
+  top: 48px;
+  left: 560px;
+}
+
+.titleBar {
+  position:absolute;
+  top: 48px;
+  left: 200px;
+}
+
+.closeWin {
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  top:48px;
+  right: 85px;
 }
 </style>
